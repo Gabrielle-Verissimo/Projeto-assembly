7c993a9d912fd9143ad5fce5af2038a1ca5d10e7
Gabrielle Verissimo <gabriellev007@gmail.com>
2023-11-04 01:32:58 -0300

    Projeto finalizado versão 1.0.0

--- backup.asm
+++ backup.asm
@@ -1,56 +1,14 @@
-read_write_loop:
-    ; Ler uma linha da imagem
-    invoke ReadFile, fileHandle, addr lineImg, aux, addr readCount, NULL
-    cmp y, ecx
-    je read_final 
-    mov edx, BYTE PTR lineImg[y]
-    censor:
-        cmp z, ebx
-        je continue
-        mov eax, BYTE PTR edx[x + z]
-        mov [eax+0], 0
-        mov [eax+1], 0
-        mov [eax+2], 0
-        
-        add z, 3
-        jmp censor
-
-    continue:
-    ; Escrever a linha lida no arquivo de destino
-    invoke WriteFile, writeHandle, addr lineImg, aux, addr writeCount, NULL
-    add y, 3
-    jmp read_write_loop
-
-read_final:
-    cmp readCount, 0
-    je exit_program
-    invoke ReadFile, fileHandle, addr lineImg, aux, addr readCount, NULL
-    invoke WriteFile, writeHandle, addr lineImg, aux, addr writeCount, NULL
-    jmp read_final
+    printY db "Digite um valor para y:", 0
+    printWidth db "\nDigite um valor para a largura:", 0
+    printHeight db "\nDigite um valor para a altura:", 0
+    invoke WriteConsole, outputHandle, addr printInfo, 62, addr consoleCount, NULL
+    invoke ReadConsole, inputHandle, addr x, sizeof x, addr consoleCount, NULL
     
-y_loop:
-   ;invoke WriteConsole, outputHandle, addr fileName, sizeof fileName, addr consoleCount, NULL
-   cmp y, 30
-   je read_final
-   invoke ReadFile, fileHandle, addr lineImg, aux, addr readCount, NULL
-   mov ecx, y
-   mov eax, offset lineImg
-   mov ebx, [eax][ecx]
-   ;printf("entrou aqui")
-   jmp censor
+    invoke WriteConsole, outputHandle, addr printY, 24, addr consoleCount, NULL
+    invoke ReadConsole, inputHandle, addr y, sizeof y, addr consoleCount, NULL
 
-censor:
-    cmp x, 480
-    je write_file  
-    mov al, 0
-    mov edx, x
-    mov [ebx][edx], al
-    ;mov byte ptr [ebx+edx], al
-    add x, 1
-    jmp censor
+    invoke WriteConsole, outputHandle, addr printWidth, 32, addr consoleCount, NULL
+    invoke ReadConsole, inputHandle, addr widthInput, sizeof widthInput, addr consoleCount, NULL
     
-write_file:
-    invoke WriteFile, writeHandle, addr lineImg, aux, addr writeCount, NULL
-    sub y, 1
-    jmp y_loop
-
+    invoke WriteConsole, outputHandle, addr printHeight, 31, addr consoleCount, NULL
+    invoke ReadConsole, inputHandle, addr heightInput, sizeof heightInput, addr consoleCount, NULL
--- projeto.asm
+++ projeto.asm
@@ -30,18 +30,44 @@ include \masm32\macros\macros.asm
     newFile db 20 dup(0)
     newFilePrint db "Nome do arquivo novo:", 0 
     writeCount dd 0
-    widthInput dd 0
-    heightInput dd 0
-    x dd 0
-    y dd 0
-    value dd 0
-    aux dd 0
-    widthFinal dd 0
-    countY dd 0
-    countX dd 0
-
+    widthInput db 4 dup(0)
+    heightInput db 5 dup(0)
+    widthDD dd 0
+    heightDD dd 0
+    x db 4 dup(0)
+    y db 4 dup(0)
+    coordinateX dd 0
+    coordinateY dd 0
+    sizeLineImg dd 0
+    widthTotal dd 0
+    heightCopy dd 0
+    heightTotal dd 0
 .code
-    ;censor:
+    ;Funcao de censura
+    censor:
+        ; Verificar se atingimos o fim da largura
+        push ebp
+        mov ebp, esp
+        
+        mov eax, DWORD PTR [ebp+16]
+
+        paint:
+            ; Preencher com a cor preta (0, 0, 0) no padrao RGB
+            cmp  DWORD PTR [ebp+12], eax
+            je return_func
+            mov ebx, [ebp+8]
+            mov ecx, DWORD PTR [ebp+12]
+            mov BYTE PTR [ebx][ecx], 0
+            mov BYTE PTR [ebx][ecx+1], 0
+            mov BYTE PTR [ebx][ecx+2], 0
+            ; Avancar para o proximo pixel
+            add DWORD PTR [ebp+12], 3
+            jmp paint
+
+        return_func:
+        mov esp, ebp
+        pop ebp
+        ret 12
         
 start:
     invoke GetStdHandle, STD_INPUT_HANDLE
@@ -53,41 +79,103 @@ start:
     invoke WriteConsole, outputHandle, addr filePrint, 20, addr consoleCount, NULL
     invoke ReadConsole, inputHandle, addr fileName, sizeof fileName, addr consoleCount, NULL
 
-    ;;; Pede a entrada do x,y, width e heigth
     invoke WriteConsole, outputHandle, addr printInfo, 61, addr consoleCount, NULL
     invoke ReadConsole, inputHandle, addr x, sizeof x, addr consoleCount, NULL
+    mov esi, offset x ; Armazenar apontador da string em esi
+clear_x:
+    mov al, [esi] ; Mover caracter atual para al
+    inc esi ; Apontar para o proximo caracter
+    cmp al, 48 ; Verificar se menor que ASCII 48 - FINALIZAR
+    jl convert_x
+    cmp al, 58 ; Verificar se menor que ASCII 58 - CONTINUAR
+    jl clear_x
+; Converte x de string para inteiro
+convert_x:
+    dec esi ; Apontar para caracter anterior
+    xor al, al ; 0 ou NULL
+    mov [esi], al ; Inserir NULL logo apos o termino do numero
+    invoke atodw, addr x
+    mov coordinateX, eax
+   
     invoke ReadConsole, inputHandle, addr y, sizeof y, addr consoleCount, NULL
+    mov esi, offset y
+clear_y:
+    mov al, [esi] ; Mover caracter atual para al
+    inc esi ; Apontar para o proximo caracter
+    cmp al, 48 ; Verificar se menor que ASCII 48 - FINALIZAR
+    jl convert_y
+    cmp al, 58 ; Verificar se menor que ASCII 58 - CONTINUAR
+    jl clear_y
+; Converte y de string para inteiro
+convert_y:
+    dec esi ; Apontar para caracter anterior
+    xor al, al ; 0 ou NULL
+    mov [esi], al ; Inserir NULL logo apos o termino do numero
+    invoke atodw, addr y
+    mov coordinateY, eax
+
     invoke ReadConsole, inputHandle, addr widthInput, sizeof widthInput, addr consoleCount, NULL
+    mov esi, offset widthInput
+clear_width:
+    mov al, [esi] ; Mover caracter atual para al
+    inc esi ; Apontar para o proximo caracter
+    cmp al, 48 ; Verificar se menor que ASCII 48 - FINALIZAR
+    jl convert_width
+    cmp al, 58 ; Verificar se menor que ASCII 58 - CONTINUAR
+    jl clear_width
+; Converte width de string para inteiro
+convert_width:
+    dec esi ; Apontar para caracter anterior
+    xor al, al ; 0 ou NULL
+    mov [esi], al ; Inserir NULL logo apos o termino do numero
+    invoke atodw, addr widthInput
+    mov widthDD, eax
+
     invoke ReadConsole, inputHandle, addr heightInput, sizeof heightInput, addr consoleCount, NULL
+    mov esi, offset heightInput
+clear_height:
+    mov al, [esi] ; Mover caracter atual para al
+    inc esi ; Apontar para o proximo caracter
+    cmp al, 48 ; Verificar se menor que ASCII 48 - FINALIZAR
+    jl convert_height
+    cmp al, 58 ; Verificar se menor que ASCII 58 - CONTINUAR
+    jl clear_height
+; Converte height de string para inteiro
+convert_height:
+    dec esi ; Apontar para caracter anterior
+    xor al, al ; 0 ou NULL
+    mov [esi], al ; Inserir NULL logo apos o termino do numero
+    invoke atodw, addr heightInput
+    mov heightDD, eax
     
     mov esi, offset fileName ; Armazenar apontador da string em esi
-proximo1:
+clear_file:
     mov al, [esi] ; Mover caractere atual para al
     inc esi ; Apontar para o proximo caractere
     cmp al, 13 ; Verificar se eh o caractere ASCII CR - FINALIZAR
-    jne proximo1
+    jne clear_file
     dec esi ; Apontar para caractere anterior
     xor al, al ; ASCII 0
     mov [esi], al ; Inserir ASCII 0 no lugar do ASCII CR
 
-;;; Abre o arquivo ;;;
+    ;;; Abre o arquivo ;;;
     invoke CreateFile, addr fileName, GENERIC_READ, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL
     mov fileHandle, eax
     invoke ReadFile, fileHandle, addr eighteenBytes, 18, addr readCount, NULL
     invoke ReadFile, fileHandle, addr widthImg, 4, addr readCount, NULL
     invoke ReadFile, fileHandle, addr finalHeader, 32, addr readCount, NULL
-
+    
     ;;; Pede para inserir o nome do novo arquivo ;;;
     invoke WriteConsole, outputHandle, addr newFilePrint, 22, addr consoleCount, NULL
     invoke ReadConsole, inputHandle, addr newFile, sizeof newFile, addr consoleCount, NULL
  
 
 mov esi, offset newFile ; Armazenar apontador da string em esi
-proximo2:
+clear_new_file:
     mov al, [esi] ; Mover caractere atual para al
     inc esi ; Apontar para o proximo caractere
     cmp al, 13 ; Verificar se eh o caractere ASCII CR - FINALIZAR
-    jne proximo2
+    jne clear_new_file
     dec esi ; Apontar para caractere anterior
     xor al, al ; ASCII 0
     mov [esi], al ; Inserir ASCII 0 no lugar do ASCII CR
@@ -95,67 +183,74 @@ proximo2:
 
     invoke CreateFile, addr newFile, GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL ; cria um novo arquivo para escrita
     mov writeHandle, eax ; ponteiro para novo arquivo
+
     ;;; Le os primeiros 18 bytes, depois o tamanho da largura e por ultimo os 32 bytes restante do cabe?alho ;;; 
     invoke WriteFile, writeHandle, addr eighteenBytes, 18, addr writeCount, NULL
     invoke WriteFile, writeHandle, addr widthImg, 4, addr writeCount, NULL
     invoke WriteFile, writeHandle, addr finalHeader, 32, addr writeCount, NULL
     
+    ;multiplica o widht por 3 (pixel equivalente a 3 bytes)
     mov eax, 3
     mul widthImg
-    mov aux, eax
-    mov countY, 0
-    mov countX, 750
-    ;invoke WriteConsole, outputHandle, addr x, 4, addr consoleCount, NULL
-    ;invoke WriteConsole, outputHandle, addr y, 4, addr consoleCount, NULL
-    ;invoke WriteConsole, outputHandle, addr widthInput, 4, addr consoleCount, NULL
-    ;invoke WriteConsole, outputHandle, addr heightInput, 4, addr consoleCount, NULL
-    ;invoke WriteConsole, outputHandle, addr countY, 4, addr consoleCount, NULL
+    mov sizeLineImg, eax
+    
+    ;soma width com coordenada x e multiplica por 3
+    mov ebx, widthDD
+    add ebx, coordinateX
+    mov widthTotal, ebx
+    mov eax, 3
+    mul widthTotal
+    mov widthTotal, eax
+    
+    ;soma altura com coordenada y
+    mov edx, heightDD
+    add edx, coordinateY
+    mov heightTotal, edx
 
+    ;faz uma copia da coordenada y para a variavel heighcopy
+    mov ecx, coordinateY
+    mov heightCopy, ecx
+    
+    ;multiplica coordenada x por 3
+    mov eax, 3
+    mul coordinateX
+    mov coordinateX, eax
+   
+;ler as linhas que não serão alteradas até encontrar a que será alterada e então desviará para o begin_censor
 read_write_loop:
-    ; Ler uma linha da imagem
-    invoke ReadFile, fileHandle, addr lineImg, aux, addr readCount, NULL
-    cmp countY, 310
-    je censor
-    invoke WriteFile, writeHandle, addr lineImg, aux, addr writeCount, NULL
-    add countY, 1
+    cmp coordinateY, 0
+    je begin_censor
+
+    ;Ler uma linha da imagem
+    invoke ReadFile, fileHandle, addr lineImg, sizeLineImg, addr readCount, NULL
+    invoke WriteFile, writeHandle, addr lineImg, sizeLineImg, addr writeCount, NULL
+    sub coordinateY, 1
     jmp read_write_loop
 
-write_file:
-    ; Escrever a linha lida no arquivo de destino
-    invoke WriteFile, writeHandle, addr lineImg, aux, addr writeCount, NULL
-    jmp label_name
-
-censor:
-    cmp countX, 1440
-    je write_file
-    mov ebx, offset lineImg
-    mov al, 0
-    mov edx, countX
-    ;mov edx, ecx
-    mov [ebx][edx], al
-    ;mov [ebx][edx+1], al
-    ;mov [ebx][edx+2], al
-    add countX, 1
-    jmp censor
-
-label_name:
-    cmp countY, 340
+;loop onde chama a funcao de censura até que chegue na altura total que foi informada
+begin_censor:
+    ;Ler uma linha da imagem
+    invoke ReadFile, fileHandle, addr lineImg, sizeLineImg, addr readCount, NULL
+    mov edx, heightTotal
+    cmp heightCopy, edx
     je read_final
-    add countY, 1
-    mov countX, 750
-    invoke ReadFile, fileHandle, addr lineImg, aux, addr readCount, NULL
-    jmp censor
+    push widthTotal
+    push coordinateX 
+    push offset lineImg
+    call censor
+    invoke WriteFile, writeHandle, addr lineImg, sizeLineImg, addr writeCount, NULL
+    add heightCopy, 1
+    jmp begin_censor
 
+;ler as linhas que sobraram sem alteracao
 read_final:
     cmp readCount, 0
     je exit_program
-    ;invoke WriteConsole, outputHandle, addr fileName, sizeof fileName, addr consoleCount, NULL
-    invoke ReadFile, fileHandle, addr lineImg, aux, addr readCount, NULL
-    invoke WriteFile, writeHandle, addr lineImg, aux, addr writeCount, NULL
+    invoke ReadFile, fileHandle, addr lineImg, sizeLineImg, addr readCount, NULL
+    invoke WriteFile, writeHandle, addr lineImg, sizeLineImg, addr writeCount, NULL
     jmp read_final
 
 exit_program:
-   
     invoke CloseHandle, fileHandle
     invoke CloseHandle, writeHandle
     invoke ExitProcess, 0
